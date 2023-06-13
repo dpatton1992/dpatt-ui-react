03f25a28740b9d8f7efc3dcbfacbe6da4e4cf02d
Daniel Patton <65815343+dpatton1992@users.noreply.github.com>
2023-06-13 13:47:59 -0400

    chore: update menu config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,25 +3,27 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'My Docs',
-      social: {
-        github: 'https://github.com/withastro/starlight',
-      },
-      sidebar: [
-        {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
-          ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
-        },
-      ],
-    }),
-  ],
+	integrations: [
+		starlight({
+			title: 'DPatt-UI',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Get Started',
+					autogenerate: { directory: 'get-started' },
+					// items: [
+					// 	// Each item here is one entry in the navigation menu.
+					// 	{ label: 'Installation', link: '/get-started/installation/' },
+					// 	{ label: 'Usage', link: '/get-started/usage/' },
+					// ],
+				},
+				{
+					label: 'Components',
+					autogenerate: { directory: 'components' },
+				},
+			],
+		}),
+	],
 });
